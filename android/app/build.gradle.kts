8836848e7bc4f6afca6e9e073069b2edc941f834
Alejandro Rueda <alecz23@live.com>
2026-09-08 13:38:42 -0600

    fix: add AndroidX media dependency for widget controls

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
     }
 }
 
+dependencies {
+    implementation("androidx.media:media:1.7.0")
+}
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
