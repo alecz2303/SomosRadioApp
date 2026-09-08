fe7c4a59038ee925ac5e7526a2edcf7635fbf8f5
Alejandro Rueda <alecz23@live.com>
2026-09-07 21:13:10 -0600

    feat: initialize background radio service

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 import 'app/somos_radio_app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.alecz.somos_radio.audio',
+    androidNotificationChannelName: 'Somos Radio',
+    androidNotificationOngoing: true,
+  );
+
   runApp(const SomosRadioApp());
 }
