2bdf0a1145a74371ee63acf628ccf1dfbfc21ee7
Alejandro Rueda <alecz23@live.com>
2026-09-08 12:56:53 -0600

    refactor: bootstrap shared radio audio service

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 
 import 'app/somos_radio_app.dart';
+import 'core/audio/audio_service_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await JustAudioBackground.init(
-    androidNotificationChannelId: 'com.alecz.somos_radio.audio',
-    androidNotificationChannelName: 'Somos Radio',
-    androidNotificationOngoing: true,
-  );
-
+  await initializeRadioAudioService();
   runApp(const SomosRadioApp());
 }
