3f9f80b30809551a98ce5ac86db297aa5993e3db
Alejandro Rueda <alecz23@live.com>
2026-09-14 23:03:28 -0600

    feat: use native Media3 session on Android

--- lib/core/audio/audio_service_manager.dart
+++ lib/core/audio/audio_service_manager.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
+
 import 'package:audio_service/audio_service.dart';
 
+import 'native_media3_audio_handler.dart';
 import 'radio_audio_handler.dart';
 
 late final AudioHandler radioAudioHandler;
 
 Future<void> initializeRadioAudioService() async {
+  if (Platform.isAndroid) {
+    radioAudioHandler = NativeMedia3AudioHandler();
+    return;
+  }
+
   radioAudioHandler = await AudioService.init(
     builder: RadioAudioHandler.new,
     config: const AudioServiceConfig(
